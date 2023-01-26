e05b1d8d4dcb12c2f4afda821e76d97a61dea052
salzate <salzategarcia@gmail.com>
2023-01-26 08:49:35 -0500

    ok

--- src/containers/login.jsx
+++ src/containers/login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/login.scss";
-import logo from '../../public/assets/logos/logo_yard_sale.svg';
+import logo from '../logos/logo_yard_sale.svg';
 
 const Login = () => {
   return (
